9028c793bda454dad044933d8ebb87079ae64efd
ConnerKT <connerthompson121@gmail.com>
2024-05-08 11:56:12 -0500

    practice with inserting one and many

--- main.go
+++ main.go
@@ -1,36 +1,54 @@
 package main
 
-
 import (
 	"context"
 	"fmt"
-  
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-  )
-  
-  func main() {
-	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://<username>:<password>@cluster0.nnbdzbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
-  
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), opts)
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	//Setting my atlas db to a variable to reuse later (env to be made)
+	var atlasDb = "mongodb+srv://golang_user:password1234@cluster0.nnbdzbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	fmt.Println(atlasDb)
+	//Anytime you make requests to a server (our db),
+	//you should create a context using context.TODO()
+	//that the server will accept
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	usersCollection := client.Database("testing").Collection("users")
+	// insert a single document into a collection
+	// create a bson.D object
+	// user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+	// insert the bson object using InsertOne()
+	// result, err := usersCollection.InsertOne(context.TODO(), user)
+	// check for errors in the insertion
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // display the id of the newly inserted object
+	// fmt.Println(result.InsertedID)
+
+	// insert multiple documents into a collection
+	// create a slice of bson.D objects
+	users := []interface{}{
+		bson.D{{"fullName", "User 2"}, {"age", 25}},
+		bson.D{{"fullName", "User 3"}, {"age", 20}},
+		bson.D{{"fullName", "User 4"}, {"age", 28}},
+	}
+	// insert the bson object slice using InsertMany()
+	results, err := usersCollection.InsertMany(context.TODO(), users)
+	// check for errors in the insertion
 	if err != nil {
-	  panic(err)
-	}
-  
-	defer func() {
-	  if err = client.Disconnect(context.TODO()); err != nil {
 		panic(err)
-	  }
-	}()
-  
-	// Send a ping to confirm a successful connection
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	  panic(err)
 	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-  }
-  
+	// display the ids of the newly inserted objects
+	fmt.Println(results.InsertedIDs)
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+
+	}
+	if err != nil {
+		panic(err)
+	}
+}

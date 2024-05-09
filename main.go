9b17ac0193a88eccce84edd4d86f086966697bf4
ConnerKT <connerthompson121@gmail.com>
2024-05-09 11:42:49 -0500

    inserting json into db

--- main.go
+++ main.go
@@ -2,53 +2,80 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+type User struct {
+	FullName string `json:"fullName"`
+	Age      int    `json:"age"`
+}
+
+func readJSONFile(filename string) ([]User, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []User
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
 func main() {
-	//Setting my atlas db to a variable to reuse later (env to be made)
-	var atlasDb = "mongodb+srv://golang_user:password1234@cluster0.nnbdzbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-	fmt.Println(atlasDb)
-	//Anytime you make requests to a server (our db),
-	//you should create a context using context.TODO()
-	//that the server will accept
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	// Setting the connection string for MONGO
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connecting
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//disconnect if error
+	defer client.Disconnect(context.TODO())
+
+	// Check the connection
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Fatal("MongoDB connection error:", err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	// Access the database and collection if not create
 	usersCollection := client.Database("testing").Collection("users")
-	// insert a single document into a collection
-	// create a bson.D object
-	// user := bson.D{{"fullName", "User 1"}, {"age", 30}}
-	// insert the bson object using InsertOne()
-	// result, err := usersCollection.InsertOne(context.TODO(), user)
-	// check for errors in the insertion
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // display the id of the newly inserted object
-	// fmt.Println(result.InsertedID)
 
-	// insert multiple documents into a collection
-	// create a slice of bson.D objects
-	users := []interface{}{
-		bson.D{{"fullName", "User 2"}, {"age", 25}},
-		bson.D{{"fullName", "User 3"}, {"age", 20}},
-		bson.D{{"fullName", "User 4"}, {"age", 28}},
-	}
-	// insert the bson object slice using InsertMany()
-	results, err := usersCollection.InsertMany(context.TODO(), users)
-	// check for errors in the insertion
+	// Read users from JSON file
+	users, err := readJSONFile("data.json")
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to read JSON file:", err)
 	}
-	// display the ids of the newly inserted objects
-	fmt.Println(results.InsertedIDs)
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
 
+	// Prepare documents for insertion
+	var documents []interface{}
+	for _, user := range users {
+		documents = append(documents, bson.D{
+			{"fullName", user.FullName},
+			{"age", user.Age},
+		})
 	}
+
+	// Insert documents into MongoDB
+	insertResult, err := usersCollection.InsertMany(context.TODO(), documents)
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to insert documents into MongoDB:", err)
 	}
+
+	// Print inserted IDs
+	fmt.Println("Inserted IDs:", insertResult.InsertedIDs)
 }
